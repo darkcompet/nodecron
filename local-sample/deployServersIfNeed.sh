57fff6640bb40f9f357de5813dba4096056fbdf9
DarkCompet <darkcompet@gmail.com>
2023-03-15 16:52:02 +0700

    Simplify deploy bash

--- local-sample/deployServersIfNeed.sh
+++ local-sample/deployServersIfNeed.sh
@@ -1,7 +1,2 @@
 # MUST run with `bash` since this file be executed on a service.
-bash /var/www/ironsky/api-server/local/deployIfNeed.sh
-bash /var/www/ironsky/api-game/local/deployIfNeed.sh
-bash /var/www/ironsky/api-cardano/local/deployIfNeed.sh
-bash /var/www/ironsky/api-marketplace/local/deployIfNeed.sh
-bash /var/www/ironsky/api-task/local/deployIfNeed.sh
-bash /var/www/ironsky/marketplace/local/deployIfNeed.sh
+bash /var/www/cardano/cardano-node-wrapper/local/deployIfNeed.sh
